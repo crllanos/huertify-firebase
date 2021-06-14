cd0e1f59c5e3ee4b5f013f3fb0ff4a419fa9f984
Cristian Llanos <49594646+crllanos@users.noreply.github.com>
2021-06-14 15:47:25 -0400

    HTFY-25-germinacion

---
+++ functions/src/screens/04germinacion/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Text, Image, View, TextInput, ScrollView, Button, StyleSheet, Picker } from 'react-native';
+
+import Estilo from '../../constants/Estilo';
+import Plantacion, {getPlantaciones} from '../../model/plantacion'
+
+/** git checkout -b HTFY-25-germinacion */
