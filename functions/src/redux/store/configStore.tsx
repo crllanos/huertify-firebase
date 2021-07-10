405261b2164ed1e83cd68cc708456cc015e25266
Cristian Llanos <cristian.llanos@gmail.com>
2021-07-09 22:25:16 -0400

    wip react-redux

---
+++ functions/src/redux/store/configStore.tsx
@@ -0,0 +1,16 @@
+import { createStore, combineReducers } from 'redux'
+import hfplantacionReducer from '../reducers/hfplantacion'
+import filtersReducer from '../reducers/filters'
+
+// Store creation
+
+export default () =>
+{
+	const store = createStore(
+		combineReducers( {
+			hfplantacion: hfplantacionReducer
+			, filters: filtersReducer
+		} ) );
+
+	return store;
+};
