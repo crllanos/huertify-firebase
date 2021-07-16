283f582aea6f1ec71b99705b102c634dc4e4f62b
Cristian Llanos <cristian.llanos@gmail.com>
2021-07-16 14:28:27 -0400

    BUGS MANAGEMENT

--- functions/src/screens/_layout/footer.tsx
+++ functions/src/screens/_layout/footer.tsx
@@ -7,7 +7,7 @@ export default function FooterHf(){
     return (
         <View style={Estilo.contenedor_footer}>
             <Text style={Estilo.parrafo_mini}>-</Text>
-            <Text>(*) Nos falta tu planta? Avisanos!</Text>
+            <Text>(*) Nos falta tu planta? Pillaste un Bug? Avisanos!</Text>
             <Text style={Estilo.parrafo_mini}><Enlace texto="twitter.com/Huertify" donde={Config.url} /></Text>
             <Text style={[Estilo.parrafo_mini, Estilo.italic]}>Huertify.org - 2021</Text>
        </View>
